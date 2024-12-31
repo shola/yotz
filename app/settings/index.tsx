d3966510179f1f23c61fa2e9157548f8858a55f6
shola <michael.o.situ@gmail.com>
2024-12-31 12:57:58 -0800

    docs: add notes about keeping components separate from the routes

--- app/settings/index.tsx
+++ app/settings/index.tsx
@@ -1,3 +1,8 @@
 /**
  * Starter route that matches '/settings'
+ * 
+ * Routes are a a collection of reuseable components and should
+ * import and instantiate components, components should not live in
+ * routes. There should be a parallel directory structure for
+ * components.
  */ 
