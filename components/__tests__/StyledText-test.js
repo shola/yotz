4f36a934892ee8cfed3b196f9110d332f84c9673
shola <michael.o.situ@gmail.com>
2024-12-31 12:12:29 -0800

    docs: add note to create tests with deep links

--- components/__tests__/StyledText-test.js
+++ components/__tests__/StyledText-test.js
@@ -3,6 +3,12 @@ import renderer from 'react-test-renderer';
 
 import { MonoText } from '../StyledText';
 
+/**
+ * Taken from expo router docs:
+ * Deep links always work, for every page. This makes it possible to share links
+ * to any content in the app, which is great for promoting your app, collecting
+ * bug reports, E2E testing, automating screenshots, and so on.
+ */  
 it(`renders correctly`, () => {
   const tree = renderer.create(<MonoText>Snapshot test!</MonoText>).toJSON();
 
