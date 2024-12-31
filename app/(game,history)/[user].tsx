42c6e48d2e9f1522d7b7b17e570c3b6ee367fcbe
shola <michael.o.situ@gmail.com>
2024-12-31 12:51:12 -0800

    feat: enable shared routes with array syntax

---
+++ app/(game,history)/[user].tsx
@@ -0,0 +1,5 @@
+/**
+ * This is a shared route. It will create 'app/game/[user.tsx]' and 
+ * 'app/history/[user].tsx' routes instead of creating them of having
+ * to duplicate the [user].tsx route in multiple directories.
+ */ 
---
+++ app/(game,history)/_layout.tsx
@@ -0,0 +1,15 @@
+/**
+ * Enable the array syntax for shared routes with the following code
+ */ 
+
+export const unstable_settings = {
+    initialRouteName: 'game',
+    search: {
+      initialRouteName: 'history',
+    },
+  };
+  
+  export default function DynamicLayout({ segment }) {
+    //  ... 
+  }
+  
