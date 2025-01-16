43cc4bd2804022e920641265fd0d399feffc7ce1
shola <michael.o.situ@gmail.com>
2025-01-15 17:55:47 -0800

    chore: prefer to rename gameCard/gameCard.tsx to index.tsx

--- app/game.tsx
+++ app/game.tsx
@@ -1,6 +1,6 @@
 import { Text, StyleSheet, View } from "react-native";
 import { useState, createContext, SetStateAction, Dispatch } from "react";
-import GameCard from "@/components/gameCard/gameCard";
+import GameCard from "@/components/gameCard";
 import DiceControl from "@/components/dice/diceControl";
 import { DiceContext, shuffleDiceValues } from "@/components/dice/diceContext";
 import type { DieValue } from "@/components/dice/diceContext";
